00eb3da9ffabb4766fda6dc005829034cf1914c4
shinbeann <145249450+shinbeann@users.noreply.github.com>
2024-08-10 14:26:25 +0800

    Delete routes directory

--- routes/attendanceRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import markAttendance from '../controllers/attendanceController.js';
-
-const router = express.Router();
-
-router.post('/markAttendance', markAttendance);
-
-export default router;
